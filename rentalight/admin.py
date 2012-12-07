1f43fcb7737c0fd1afd13008f1a738027b27ec31
Sree <SRaja001@gmail.com>
2012-12-07 12:30:38 -0500

    Got through Django tutorial 2

---
+++ rentalight/admin.py
@@ -0,0 +1,31 @@
+from rentalight.models import Poll, Choice
+from django.contrib import admin
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+
+#admin.site.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+	fieldsets = [
+        (None,               {'fields': ['question']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	list_display = ('question', 'pub_date','was_published_recently')
+	
+	inlines = [ChoiceInline]
+	list_filter = ['pub_date']
+	search_fields = ['question']
+	date_hierarchy = 'pub_date'
+	
+	
+#class PollAdmin(admin.ModelAdmin):
+#    fieldsets = [
+#        (None,               {'fields': ['question']}),
+#        ('Date information', {'fields': ['pub_date']}),
+#    ]
+
+	
+admin.site.register(Poll, PollAdmin)
+#admin.site.register(Choice)
